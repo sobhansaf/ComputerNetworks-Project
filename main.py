7ddda73127fa699879b624cc4637c3a63babbce6
Sobhan <sobhan.saf79@gmail.com>
2020-07-21 20:43:12 +0430

    supported protocols are now printed in an infinite loop

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def icmp(data):
         'Rest of header': repr(data[4:]) 
     }
     
-    return icmp_fields
+    return (icmp_fields, None)  # because other functions return 2 objects, in order to be match with them returning a none
 
 
 def arp(data):
@@ -123,7 +123,7 @@ def arp(data):
 
     arp_fields = {
         'Hardware type': hw_type,
-        'Protocol type': proto_type,
+        'Protocol number': proto_type,
         'Hardware address length': hw_add_len,
         'Protocol address length': proto_add_len,
         'Opcode': opcode,
@@ -136,7 +136,7 @@ def arp(data):
     if current < len(data):
         arp_fields['Data'] = repr(data[current])
 
-    return arp_fields
+    return (arp_fields, None)  # because other functions return 2 objects, in order to be match with them returning a none
 
 
 def udp(data):
@@ -370,26 +370,85 @@ def http(data):
     return {'Header': header, 'Message': message}
 
 
+def make_protocol_str(headers, protocol_name):
+    if headers is None:  # sometimes output of http heaer is None:
+        return None
+    res = ''
+    res += '{:=^30}'.format(protocol_name) + '\n'
+    for item in headers:
+        res += f'{item} ==> {headers[item]}' + '\n'
+    return res
+    
+
+
 
 
 # a socket for packets recieved
 conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
+# supported protocols
+layer_two_porotocols = {2048: ip, 2054: arp}
+layer_three_protocols = {6: tcp, 17: udp, 1: icmp}
+layer_four_protocols = {80: http, 53: dns}
+
+
 while True:
     raw_data, addr = conn.recvfrom(65535)
 
-    ether_headers, data = ether(raw_data)
+    string = ''
 
-    if ether_headers['Ethertype'] == 2048: # ip
-        ip_headers, data = ip(data)
+    ether_headers, data = ether(raw_data)  
 
-        if ip_headers['Protocol number'] == 6: # tcp
-            tcp_headers, data = tcp(data)
+    if ether_headers['Ethertype'] not in layer_two_porotocols:
+        continue
 
 
-            if tcp_headers['Source port number'] == 80 or tcp_headers['Destination port number'] == 80: # http
-                data = http(data)
-                print(data)
-            
+    string += make_protocol_str(ether_headers, 'Ethernet')
+
+    layer_two_headers, data = layer_two_porotocols[ether_headers['Ethertype']](data)
+
+
+    try:
+        if layer_two_headers['Protocol number'] not in layer_three_protocols:
+            continue
+    except:
+        print('+' * 50)
+        print(ether_headers)
+        print(layer_two_headers)
+        break
+
+
+    string += make_protocol_str(layer_two_headers, layer_two_porotocols[ether_headers['Ethertype']].__name__.upper())
+
+    if data is None: # arp
+        print('-' * 50)
+        print(string)
+        print('-' * 50)
+        continue
+
+    layer_three_headers, data = layer_three_protocols[layer_two_headers['Protocol number']](data)
+
+    string += make_protocol_str(layer_three_headers, layer_three_protocols[layer_two_headers['Protocol number']].__name__.upper())
+
+    if data is None:  # icmp
+        print('-' * 50, string, '-' * 50, sep='\n')
+        continue
+
+    if layer_three_headers['Source port number'] in layer_four_protocols:
+        layer_four_headers = layer_four_protocols[layer_three_headers['Source port number']](data)
+        string += make_protocol_str(layer_four_headers, layer_four_protocols[layer_three_headers['Source port number']].__name__.upper())
+
+    
+    elif layer_three_headers['Destination port number'] in layer_four_protocols:
+        layer_four_headers = layer_four_protocols[layer_three_headers['Destination port number']](data)
+        string += make_protocol_str(layer_four_headers, layer_four_protocols[layer_three_headers['Destination port number']].__name__.upper())
+
+    
+    else:
+        continue
+
+
+    print('-' * 50, string, '-' * 50, sep='\n')
+
 
 
