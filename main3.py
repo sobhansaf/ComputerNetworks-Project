457ed5399e4ee4e33222cb3c6b26bcaecdcf6816
Sobhan <sobhan.saf79@gmail.com>
2020-09-15 19:05:53 +0430

    2 functions for converting ip and integer to each other is added. also a function for determining the first and last ip integer is added

--- main3.py
+++ main3.py
@@ -1,9 +1,9 @@
 from arp import Arp
 import optparse
+import re
 
 def get_inputs():
     import netifaces
-    import re
 
     parser = optparse.OptionParser()
     parser.add_option('-m', '--mac', help='Source MAC address (one of source mac and interface name is required. mac has more priority)', dest='mac')
@@ -27,12 +27,50 @@ def get_inputs():
 
     return mac, ip, mask
 
+def make_ip_integer(ip):
+    # gets an ip like "192.168.1.1". it is 4 bytes or 32bit. returns an integer as its corresponding unsigned integer
+    assert (re.match(r'(\d{1,3}\.){3}\d{1,3}', ip))
+    numbers = tuple(reversed(tuple(map(int, ip.split('.')))))
+    res = 0
+
+    # ip can be thought of a number in base of 256. so to make it to a decimal integer we can use a loop
+    # numbers of ip has been reversed.
+    for i in range(len(numbers)):
+        res += 256 ** i * numbers[i]
+    
+    return res
+
+def make_integer_ip(ip_integer):
+    # gets an integer. this integer is assumed to be 4 bytes. this function breaks this integer to 4 bytes each of seze 1B.
+    # and makes an ip according to that
+    assert(type(ip_integer) == int)
+    
+    res = ''
+    for i in range(4):
+        res += str(ip_integer // (256 ** (3 - i)))
+        ip_integer = ip_integer % (256 ** (3 - i))
+        res += '.'
+    res = res[:-1]  # in the above for loop there will be an additional "." at the end of string.
+    return res
+
+
 def get_range(ip, mask):
+    # ip is a string. -> "192.168.1.1". mask is an integer between 0, 32
     # gets the net id(ip) as a string and mask.
     # returns two integers. first one the start ip and second is last ip
-    # each integer is the converted version of 32 bit ip. e.g
-    pass
+    # each integer is the converted version of 32 bit ip. e.g: 192.168.1.1 -> 11000000.10100100.00000001.00000001 -> 3232235777
+    assert (type(mask) == int and type(ip) == str)
+    
+    if mask > 32 or mask < 0:
+        print('[-] Wrong mask!')
+        exit(1)
+    
+    start = (make_ip_integer(ip) // (2 ** (32 - mask))) * (2 ** (32 - mask))
+    end = start + 2 ** (32 - mask) - 1
 
-get_inputs()
+    return start, end
 
 
+
+
+print(make_integer_ip(3232235777))
