29e963ceaca8c5c97f0bb3d239b3456b3f29b194
Sobhan <sobhan.saf79@gmail.com>
2020-09-16 09:56:14 +0430

    validation for interface name is added. some bugs fixed

--- checkValues.py
+++ checkValues.py
@@ -1,4 +1,6 @@
 import re
+import netifaces
+
 
 def check_ip(ip):
     # gets an IP and validates it.
@@ -13,16 +15,21 @@ def check_ip(ip):
 
 def check_mac(mac):
     # gets a string as MAC address. validates it.
-    if type(mac) != str or not re.match(r'^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$'):
+    if type(mac) != str or not re.match(r'^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$', mac):
         return False
+    return True
 
 def check_ip_range(rip):
     # gets an IP range (i.e it has a '/' for mask) and validates it
     if type(rip) != str or not re.match(r'^(\d{1,3}\.){3}\d{1,3}\/\d{1,2}$', rip):
         return False
+    return True
     
     ip = rip[:rip.find('/')]
     return check_ip(ip)
 
-
-
+def check_iface(iface):
+    # validating NIC name.
+    if type(iface) != str or iface not in netifaces.interfaces():
+        return False
+    return True
